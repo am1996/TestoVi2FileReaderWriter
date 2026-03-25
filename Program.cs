b8233f15e511aeffe835c73f8fdd6e1bade64a5d
am1996 <ahmedmagdy92@yahoo.com>
2026-03-25 11:03:58 +0200

    vi2 reader and writer created

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Vi2Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try 
+            {
+                using (var handler = new TestoVi2Handler())
+                {
+                    // 1. Read existing data
+                    Console.WriteLine("Reading data...");
+                    var data = handler.Read("input.vi2");
+                    Console.WriteLine($"Found {data.Count} points.");
+
+                    // 2. Modify data (optional)
+                    data[0].Timestamp = DateTime.Now; 
+
+                    // 3. Write to a new file
+                    Console.WriteLine("Writing new file...");
+                    handler.Write("output.vi2", data, "MyCustomSession");
+                    Console.WriteLine("Success!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + (ex.InnerException?.Message ?? ex.Message));
+            }
+        }
+    }
+}
---
+++ Vi2Handler.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace Vi2Converter
+{
+    public class MeasurementPoint
+    {
+        public DateTime Timestamp { get; set; }
+        public double[] Values { get; set; }
+    }
+
+    public class TestoVi2Handler : IDisposable
+    {
+        private object _loader;
+        private Type _loaderType;
+        private Assembly _tcddkaAssembly;
+        private PropertyInfo _protocolsProp;
+        private MethodInfo _loadMethod;
+        private MethodInfo _saveMethod;
+
+        public TestoVi2Handler(string modelDll = "TestoModellVI2.dll", string interopDll = "Interop.Tcddka.dll")
+        {
+            if (!File.Exists(modelDll) || !File.Exists(interopDll))
+                throw new FileNotFoundException("Ensure TestoModellVI2.dll and Interop.Tcddka.dll are in the folder.");
+
+            Assembly testoAssembly = Assembly.LoadFrom(modelDll);
+            _tcddkaAssembly = Assembly.LoadFrom(interopDll);
+
+            _loaderType = testoAssembly.GetType("Testo.Model.TestVI2Loader");
+            _loader = Activator.CreateInstance(_loaderType);
+
+            // Cache Reflection info for performance
+            _protocolsProp = _loaderType.GetProperty("Protocols", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+            _loadMethod = _loaderType.GetMethod("Load", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+            _saveMethod = _loaderType.GetMethod("Save", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+        }
+
+        // --- READ LOGIC ---
+        public List<MeasurementPoint> Read(string filePath)
+        {
+            var results = new List<MeasurementPoint>();
+            _loadMethod.Invoke(_loader, new object[] { filePath });
+
+            var protocols = (IEnumerable)_protocolsProp.GetValue(_loader);
+            foreach (var p in protocols)
+            {
+                dynamic dp = p; // Uses Microsoft.CSharp (Interop.Tcddka.Protocol)
+                for (long r = 0; r < dp.Count; r++)
+                {
+                    var point = new MeasurementPoint { Values = new double[dp.NumCols - 1] };
+                    point.Timestamp = DateTime.FromOADate(Convert.ToDouble(dp.GetVal(r, (short)0)));
+
+                    for (short c = 1; c < dp.NumCols; c++)
+                    {
+                        point.Values[c - 1] = Convert.ToDouble(dp.GetVal(r, c));
+                    }
+                    results.Add(point);
+                }
+            }
+            return results;
+        }
+
+        // --- WRITE LOGIC ---
+        public void Write(string filePath, List<MeasurementPoint> data, string sessionName = "ExportedSession")
+        {
+            // Create the COM Protocol object
+            Type protocolType = _tcddkaAssembly.GetType("Tcddka.ProtocolClass");
+            dynamic protocol = Activator.CreateInstance(protocolType);
+            protocol.Name = sessionName;
+
+            foreach (var point in data)
+            {
+                // Note: Tcddka Protocol objects usually expect: Add(OADate, ArrayOfValues) 
+                // or Add(OADate, Value1, Value2...). 
+                // Adjusting to the most common Interop signature:
+                protocol.Add(point.Timestamp.ToOADate(), point.Values);
+            }
+
+            // Sync with Loader and Save
+            IList protocolList = (IList)_protocolsProp.GetValue(_loader);
+            protocolList.Clear();
+            protocolList.Add(protocol);
+
+            _saveMethod.Invoke(_loader, new object[] { filePath });
+        }
+
+        public void Dispose()
+        {
+            if (_loader is IDisposable d) d.Dispose();
+        }
+    }
+}
